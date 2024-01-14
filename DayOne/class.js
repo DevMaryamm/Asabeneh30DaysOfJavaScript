ffe941a8c94f5383485752375c52255476f900d8
maryamm <maryamadamsaliyu@gmail.com>
2024-01-14 15:34:07 +0100

    first commit

---
+++ DayOne/class.js
@@ -0,0 +1,54 @@
+//NumberOne: comments can make code readable
+//NumberTwo: Welcome to 30DaysOfJavaScript
+/*numberThree: Comments can make code readable, easy to reuse and informative*/
+//NumberFour:
+
+let Food = 'Noodles'
+let Book = "Javascript"
+let num= 28
+let Travelled = true
+let emptyValue = null
+let color
+    
+//NumberFive:
+    console.log(typeof "Maryam");
+    console.log(typeof 5);
+    console.log(typeof null);
+    console.log(typeof true);
+    console.log(typeof undefined);
+
+//NumberSix:
+
+   let Tribe
+   let School
+   let BirthDate
+   let Hobby
+
+   //NumberSeven:
+
+   let Month = January
+   let Day = 11
+   let City = Ilorin
+   let Network = Mtn
+
+   //NumberEight:
+
+    let FirstName = 'Maryam'
+    let LastName = "Adams"
+    let MaritalStatus = true
+    let Country = 'Nigeria'
+    let Age = 28 
+
+    //NumberNine:
+
+    console.log(FirstName, LastName, MaritalStatus, Country, Age);
+
+    //NumberTen:
+
+    let MyAge = "I am 28 years old"
+    let YourAge = 'You are 10 years old'
+    console.log( MyAge, YourAge);
+
+
+
+    
